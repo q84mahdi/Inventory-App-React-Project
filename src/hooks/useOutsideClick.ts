b8646a62c9c9ae2b8abddc803572a510d3dddda2
q84mahdi <ghasemi84mahdi@gmail.com>
2025-11-29 14:45:56 +0330

    Update the useOutsideClick hook to TS

---
+++ src/hooks/useOutsideClick.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef, type RefObject } from "react";
+
+export default function useOutsideClick(
+  handler: () => void,
+  listenCapturing: boolean = true
+): RefObject<HTMLElement> {
+  const ref = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    function handleClick(e: MouseEvent) {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        handler();
+      }
+    }
+
+    document.addEventListener("click", handleClick, listenCapturing);
+
+    return () =>
+      document.removeEventListener("click", handleClick, listenCapturing);
+  }, [handler, listenCapturing]);
+
+  return ref;
+}
