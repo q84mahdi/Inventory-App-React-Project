fc01688e9fcf42c6a9f6f2870e7f9169e3aff239
q84mahdi <ghasemi84mahdi@gmail.com>
2025-11-29 14:00:18 +0330

    Update AppProviders file to TS

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,7 +1,8 @@
+import type { ReactNode } from "react";
 import { CategoriesProvider } from "../contexts/CategoriesContext";
 import { ProductsProvider } from "../contexts/ProductsContext";
 
-function AppProviders({ children }) {
+function AppProviders({ children }: { children: ReactNode }) {
   return (
     <CategoriesProvider>
       <ProductsProvider>{children}</ProductsProvider>
