3ce2ff8b4b13b5a2ceb8bda1e2418a914654584d
q84mahdi <ghasemi84mahdi@gmail.com>
2025-12-06 14:26:49 +0330

    Update the Navbar component to TS

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-function Navbar() {
+const Navbar: React.FC = () => {
   return (
     <div className="h-12 flex items-center justify-center bg-slate-900 mb-6">
       <h1 className="text-slate-300 font-bold text-2xl">
@@ -6,5 +6,6 @@ function Navbar() {
       </h1>
     </div>
   );
-}
+};
+
 export default Navbar;
